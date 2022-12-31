22af8356cf2c3aaf890dd3a35bec70111ed54061
darthcurtiscass <114704942+darthcurtiscass@users.noreply.github.com>
2022-12-30 17:29:57 -0700

    created employee class and constructors

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Node application used to build a webpage with client side data.
 1. Develop tests for classes.
 2. Develop classes and export them.
 3. Import Classes and subclasses file into index, where you'll create a new Object.
-4. Import 
-3. Create new arrays.
-4. Push data/newEmployee from those arrays into a new array.
-5. Generate HTML
+4. Create new arrays.
+5. Push data/newEmployee from those arrays into a new array.
+6. Generate HTML with array of objects created by with classes and subclasses.
--- index.js
+++ index.js
@@ -1,128 +1,86 @@
 //write function to write data to the HTML file.
 const fs = require('fs')
 const inquirer = require('inquirer');
-const generateHTML = require('./utils/generateHTML.js')
+const generateHTML = require('./utils/generateHTML.js');
 
 const employees = []
 //Separate questions into separate arrays. One for engineer, one for manager, and so on...
 //Add question array to ask what type of employee they'd like to add.
 
-const questions = [
-    //create a list options and main menu with options for types of employees and an option to deny the option.
-    //create array of question objects to give terminal prompts to answer.
-    //one to obtain the type of employee
-      {
-        type: 'rawlist',
-        message: 'What type of employee would you like to add to your team?',
-        name: 'position',
-        choices: ['Manager','Engineer','Intern', "I don't want to add more team members"]
-      },
-      {
-        when: (answers) => answers.position === 'Manager',
-        type: 'input',
-        message: "What is the Manager's name?",
-        name: "name",
-      },
-      {
-        when: (answers) => answers.position === 'Manager',
-        type: 'input',
-        message: "What is the Manager's id?",
-        name: "id",
-      },
-      {
-        when: (answers) => answers.position === 'Manager',
-        type: 'input',
-        message: "What is the Manager's email?",
-        name: "email",
-      },
-      {
-        when: (answers) => answers.position === 'Manager',
-        type: 'input',
-        message: "What is the Manager's office number?",
-        name: "officeNumber",
-      },
-      {
-        when: true,
-        type: 'rawlist',
-        message: 'What type of employee would you like to add to your team?',
-        name: 'position',
-        choices: ['Manager','Engineer','Intern', "I don't want to add more team members"]
-      },
-      {
-        when: (answers) => answers.position === 'Engineer',
-        type: 'input',
-        message: "What is the Engineer's name?",
-        name: "name",
-      },
-      {
-        when: (answers) => answers.position === 'Engineer',
-        type: 'input',
-        message: "What is the Engineer's id number?",
-        name: "id",
-      },
-      {
-        when: (answers) => answers.position === 'Engineer',
-        type: 'input',
-        message: "What is the Engineer's email?",
-        name: "email"
-      },
-      {
-        when: (answers) => answers.position === 'Engineer',
-        type: 'input',
-        message: "What is the Engineer's gitHub username?",
-        name: "gitHub"
-      },
-      {
-        when: (answers) => answers.position === 'Engineer',
-        type: 'rawlist',
-        message: 'What type of employee would you like to add to your team?',
-        name: 'position',
-        choices: ['Manager','Engineer','Intern', "I don't want to add more team members"]
-      },
-      {
-        when: (answers) => answers.position === 'Intern',
-        type: 'input',
-        message: "What is the Intern's name?",
-        name: "name",
-      },
-      {
-        when: (answers) => answers.position === 'Intern',
-        type: 'input',
-        message: "What is the Intern's id?",
-        name: "id",
-      },
-      {
-        when: (answers) => answers.position === 'Intern',
-        type: 'input',
-        message: "What is the Intern's email?",
-        name: "email",
-      },
-      {
-        when: (answers) => answers.position === 'Intern',
-        type: 'input',
-        message: "Where does the Intern's attend school?",
-        name: "school",
-      },
-      {
-        when: (answers) => answers.position === 'Intern',
-        type: 'rawlist',
-        message: 'What type of employee would you like to add to your team?',
-        name: 'position',
-        choices: ['Manager','Engineer','Intern', "I don't want to add more team members"]
-      },
+const managerQuestions = [
+  {
+    type: 'input',
+    message: "What is the Manager's name?",
+    name: "name",
+  },
+  {
+    type: 'input',
+    message: "What is the Manager's id?",
+    name: "id",
+  },
+  {
+    type: 'input',
+    message: "What is the Manager's email?",
+    name: "email",
+  },
+  {
+    type: 'input',
+    message: "What is the Manager's office number?",
+    name: "officeNumber",
+  },
 ]
 
-// const managerQuestions = () => {
-//     if(position === 'Manager') {
-//         return [
-//             {
-//                 type: "input",
-//                 message: 'What is your favorite color?',
-//                 name: 'test',
-//             }
-//         ]
-//     }
-// }
+const engineerQuestions = [
+  {
+    when: (answers) => answers.position === 'Engineer',
+    type: 'input',
+    message: "What is the Engineer's name?",
+    name: "name",
+  },
+  {
+    when: (answers) => answers.position === 'Engineer',
+    type: 'input',
+    message: "What is the Engineer's id number?",
+    name: "id",
+  },
+  {
+    when: (answers) => answers.position === 'Engineer',
+    type: 'input',
+    message: "What is the Engineer's email?",
+    name: "email"
+  },
+  {
+    when: (answers) => answers.position === 'Engineer',
+    type: 'input',
+    message: "What is the Engineer's gitHub username?",
+    name: "gitHub"
+  },
+]
+
+const internQuestions = [
+  {
+    when: (answers) => answers.position === 'Intern',
+    type: 'input',
+    message: "What is the Intern's name?",
+    name: "name",
+  },
+  {
+    when: (answers) => answers.position === 'Intern',
+    type: 'input',
+    message: "What is the Intern's id?",
+    name: "id",
+  },
+  {
+    type: 'input',
+    message: "What is the Intern's email?",
+    name: "email",
+  },
+  {
+    type: 'input',
+    message: "Where does the Intern's attend school?",
+    name: "school",
+  },
+]
 
 
 function writeHTML() {
@@ -133,18 +91,18 @@ function writeHTML() {
             fs.writeFile("index.HTML", generateHTML(data), (err) =>
       err ? console.log(err) : console.log('HTML created!'));
         })
+        const Manager = new Manager(data.name, data.officeNumber)
 }
 
 function init() {
   inquirer
-    .prompt(ManagerQuestions)
+    .prompt(managerQuestions)
 
     .then((data)) => {
-      //push answers from question array to a new array
+      //push answers from question array to employee array
       //create new Manager with answers from questions.
     }
 }
 
 writeHTML();
 
-const Manager = new Manager(data.name, data.officeNumber)
