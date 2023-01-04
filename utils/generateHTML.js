aa1527bbe129568c0f57e89d9e3d69c40e557578
darthcurtiscass <114704942+darthcurtiscass@users.noreply.github.com>
2023-01-04 12:46:29 -0700

    completed construction of application. Tests fully functional and README almost completed.

--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -4,11 +4,11 @@ const Manager = require('../lib/Manager.subclass');
 //Creates cards for each employee with each type's shared common elements, such as: name, email, id. 
 generateCard = (data) => {
   return ` 
-  <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">${data.name}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">${data.getRole()}</h6>
-    <p class="card-text">Employee ID: ${data.id}</p>
+  <div class="card" style="width: 18rem; background-color:rgb(168, 110, 108);">
+  <div class="card-body" style="color:black;">
+    <h5 class="card-title">${data.getRole()}</h5>
+    <h6 class="card-subtitle mb-2">${data.name}</h6>
+    <p class="card-text text-muted">Employee ID: ${data.id}</p>
     <a href="mailto:${data.email}" class="card-link">${data.email}</a>
     ${generateUnique(data)}
   </div>
@@ -18,16 +18,13 @@ generateCard = (data) => {
 //Creates a line of text or link that is unique to the specific type of employee.
 generateUnique = (data) => {
   if(data.getRole() == "Manager") {
-    return`
-    <h6 class="card-subtitle mb-2 text-muted">Office Number: ${data.officeNumber}</h6>`
+    return`<h6 class="card-subtitle mb-2 text-muted">Office Number: ${data.officeNumber}</h6>`
   }
   if(data.getRole() == "Engineer") {
-    return `
-    <a href="https://github.com/${data.github}" class="card-link">Github account: ${data.github}</a>`
+    return `<a href="https://github.com/${data.github}" class="card-link">Github account: ${data.github}</a>`
   }
   if(data.getRole() == "Intern") {
-    return `
-    <h6 class="card-subtitle mb-2 text-muted">Education: ${data.school}</h6>`
+    return `<h6 class="card-subtitle mb-2">Education: ${data.school}</h6>`
   }
 }
 
@@ -44,7 +41,8 @@ generateHTML = (data) => {
     <title>Team Maker</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   </head>
-<body>
+<body style="background-color: darkgray; font-family: Georgia, 'Times New Roman', Times, serif;">
+<h1>Team Maker</h1>
 ${data.map(generateCard)}
 </body>
 `
